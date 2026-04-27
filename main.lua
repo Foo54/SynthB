7c49eca016f5d2aa94fd31821f6d3208f1d1d204
Foo45 <rowan.stjames.cooper@gmail.com>
2026-04-27 19:49:16 -0400

    turn on debug

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 SynthB = SynthB or {}
 
 SynthB.mod = SMODS.current_mod
-SynthB.DEBUG = false
+SynthB.DEBUG = true
 
 --- Prefaces print statements with some information, and doesn't do anything if debug is off
 function SynthB.debug(...) 
