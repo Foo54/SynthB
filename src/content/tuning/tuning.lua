ce109066aac533c035e25281a9404d5d96b29707
Foo45 <rowan.stjames.cooper@gmail.com>
2026-05-05 22:27:57 -0400

    changed collection rows

--- src/content/tuning/tuning.lua
+++ src/content/tuning/tuning.lua
@@ -2,7 +2,7 @@ SMODS.ConsumableType{
 	key = "Tuning",
 	primary_colour = HEX("FF0000"),
 	secondary_colour = HEX("00FFFF"),
-	collection_rows = {5, 5},
+	collection_rows = {6, 6},
 	shop_rate = 4,
 	default = "c_synthb_tuning_pitch_bend"
 }
