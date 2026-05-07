efc021d7dd5bab283dc1ee4a10e90bc2bfd88e02
Foo45 <rowan.stjames.cooper@gmail.com>
2026-05-07 07:39:36 -0400

    delay sprites for parry

--- src/api/ownership.lua
+++ src/api/ownership.lua
@@ -2,7 +2,7 @@ SMODS.Enhancement:take_ownership('glass', {
 	calculate = function(self, card, context)
 		if context.destroy_card and context.cardarea == G.play and context.destroy_card == card and SMODS.pseudorandom_probability(card, 'glass', 1, card.ability.extra) then
 			if SynthB.effect.parry() then
-				card:set_ability("m_gold")
+				card:set_ability("m_gold", nil, true)
 			else
 				card.glass_trigger = true
 				return { remove = true }
