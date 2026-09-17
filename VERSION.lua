3418db4245769dacd48815c34ff49caa7d4bfbcd
Foo45 <rowan.stjames.cooper@gmail.com>
2026-09-17 14:15:41 -0400

    last booster pack

--- VERSION.lua
+++ VERSION.lua
@@ -1,1 +1,1 @@
-return "SynthB-1.8.1-260917c"
+return "SynthB-1.8.1-260917d"
--- src/content/booster.lua
+++ src/content/booster.lua
@@ -221,3 +221,35 @@ SMODS.Booster{
 		}
 	end,
 }
+
+SMODS.Booster{
+	key = "diva_mega_2",
+	weight = 0.75,
+	kind = "synthb_diva",
+	cost = 6,
+	pos = {x=3, y=2},
+	atlas = "booster",
+	group_key = "k_worm_diva_pack",
+	config = {extra = 4, choose = 2},
+	synthb_credits = {
+		Artist = "aikoyori",
+	},
+	loc_vars = function(self, info_queue, card)
+		local cfg = (card and card.ability) or self.config
+		return {
+---@diagnostic disable-next-line: need-check-nil
+			vars = { cfg.choose, cfg.extra, localize("k_synthb_mega") },
+			key = self.key:sub(1, -8)
+		}
+	end,
+	ease_background_colour = SynthB.ease_background_colour_diva_pack,
+	particles = SynthB.diva_pack_particles,
+	create_card = function(self, card, i)
+		return {
+			attributes = {"vocaloid song"},
+			area = G.pack_cards,
+			skip_materialize = true,
+			--soulable = true,
+		}
+	end,
+}
